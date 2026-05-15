f6317398a255ef9e1f9284ccdb4da9237e5cc2ef
lsomacarrera-cell <l.somacarrera@gmail.com>
2026-05-15 15:19:26 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,41 @@
-const CACHE_NAME = "app-alhama-v1";
+const CACHE_NAME = "app-alhama-v2";
 
 const urlsToCache = [
   "./",
   "./index.html",
   "./styles.css",
   "./app.js",
-  "./manifest.json"
+  "./manifest.json",
+  "./alhama-song.mp3"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
